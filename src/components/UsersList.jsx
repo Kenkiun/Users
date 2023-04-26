1a82eb216be1a5f1c4a2c7546ddd8e11120e1a82
Julio <kennkiunn@gmail.com>
2023-04-26 16:31:03 -0500

    User

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import UserCard from './UserCard'
 
-const UsersList = ({users, deleteUSer, handleClickEdit}) => {
+const UsersList = ({users, deleteUser, handleClickEdit}) => {
 
   return (
     
     <section className='grid gap-10 auto-rows-auto grid-cols-[repeat(auto-fill,_250px)] justify-center'>
       {
-        users.map((user) => <UserCard key={user.id} deleteUSer={deleteUSer} user={user} handleClickEdit={handleClickEdit}/>)
+        users.map((user) => <UserCard key={user.id} deleteUser={deleteUser} user={user} handleClickEdit={handleClickEdit}/>)
       }
     </section>
 
